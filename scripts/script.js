cfae90ed96889eeae3ef89f92199360bde9ab1b4
Eduardo Barrera <ed.barrerat@duocuc.cl>
2026-09-03 00:55:58 -0400

    avance 3

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+function inyectarHeader(){
+    document.getElementById("header").innerHTML = '<div class="logo"><img src="https://static.wikia.nocookie.net/myl-tcg/images/f/f4/Myl-logo1-sf.png/revision/latest?cb=20240717144516&path-prefix=es" alt="icono tienda"></div><nav class="menu"><ul><li><a href="/index.html">Inicio</a></li><li><a href="/paginas/tienda.html">Productos</a></li><li><a href="/paginas/nosotros.html">Nosotros</a></li><li><a href="/paginas/blogs.html">Noticias</a></li><li><a href="/paginas/contacto.html">Contacto</a></li><li><a href="/paginas/login.html">Iniciar Sesión</a></li><li><a href="/paginas/registro.html">Registrarse</a></li></ul></nav>';
+}
+
+inyectarHeader();
